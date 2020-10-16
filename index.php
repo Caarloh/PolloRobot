5741919892dbe406c9bf532ca65ab6b1d7f7bd42
Leo Miranda <leomir2656@gmail.com>
2020-10-16 14:47:28 -0300

    Esqueleto del proyecto y creación de vistas

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php require_once "menu/parte_superior.php"?>
+
+<!--INICIO del cont principal-->
+<div class="container">
+    <h1>Pantalla De navegacion entre interfaces</h1>
+
+    
+
+   
+
+
+</div>
+<!--FIN del cont principal-->
+
+<?php require_once "menu/parte_inferior.php"?>
