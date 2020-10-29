85b449ba751a80eccfa49dd673d8bfed2e8214ef
Leo Miranda <leomir2656@gmail.com>
2020-10-29 18:25:35 -0300

    Tablero de tareas v1.0
    se pueden agregar tareas en el HTML, sin conexion a la base de datos

--- componentesVistaUsuario/usuario_superior.php
+++ componentesVistaUsuario/usuario_superior.php
@@ -12,6 +12,7 @@
   <title>Pollo Robot</title>
 
   <!-- Custom fonts for this template-->
+  <link rel="stylesheet" href="../css/tablero.css">
   <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   
@@ -21,9 +22,7 @@
 <!-- Latest compiled and minified JavaScript -->
   <!-- Custom styles for this template-->
   <link href="../css/sb-admin-2.min.css" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="../vendor/datatables/datatables.min.css"/>
-  <link rel="stylesheet" type="text/css" href="../vendor/datatables/DataTables-1.10.18/css/dataTables.bootstrap4.min.css"/>
-  
+ 
 </head>
 
 <body id="page-top">
@@ -35,11 +34,8 @@
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
       <!-- Sidebar - Brand -->
       <a class="sidebar-brand d-block align-items-center justify-content-center" href="admin.php">
-        <div class="sidebar-brand-icon">
-        <i class="fas fa-robot"></i>
-        </div>
-        <div class="sidebar-brand-text ">Pollo Robot<sup></sup></div>
-        
+        <img class="img" src="https://1.bp.blogspot.com/-i2uV-KM_sJ4/X5Dmw-sOEQI/AAAAAAAACNU/cLtL_TM4K1UotOfNzx83DnP-L58GDSOnACLcBGAsYHQ/s300/LOGOTIPO.png">
+        <br>
       </a>
         <br>
       <!-- Divider -->
@@ -76,31 +72,18 @@
        
       <!-- Divider -->
       <hr class="sidebar-divider d-none d-md-block">
-      <!-- Sidebar Toggler (Sidebar) -->
-      <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-      </div>
+     
     </ul>
     <!-- End of Sidebar -->
     <div id="content-wrapper" class="d-flex flex-column">
      <!-- Topbar -->
      <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-<!-- Sidebar Toggle (Topbar) -->
-<button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-  <i class="fa fa-bars"></i>
-</button>
+    <!-- Sidebar Toggle (Topbar) -->
+    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+      <i class="fa fa-bars"></i>
+    </button>
 
-<!-- Topbar Search -->
-<form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-  <div class="input-group">
-    <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-    <div class="input-group-append">
-      <button class="btn btn-primary" type="button">
-        <i class="fas fa-search fa-sm"></i>
-      </button>
-    </div>
-  </div>
-</form>
+
 
 <!-- Topbar Navbar -->
 <ul class="navbar-nav ml-auto">
